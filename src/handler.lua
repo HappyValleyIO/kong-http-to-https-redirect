32d3825b953f2e592f6a78d254e615a4ab2286ac
Dirk Steinkopf <dirk@steinkopf.net>
2018-11-15 10:47:05 +0100

    add config parameter exclude_uri_pattern

--- src/handler.lua
+++ src/handler.lua
@@ -11,7 +11,10 @@ function HttpFilterHandler:access(conf)
   HttpFilterHandler.super.access(self)
 
   if ngx.var.https ~= "on" then
-    return ngx.redirect("https://" .. ngx.var.host .. ngx.var.request_uri, ngx.HTTP_MOVED_PERMANENTLY) 
+    local matches_exclude_pattern = conf.exclude_uri_pattern and string.find(ngx.var.request_uri, conf.exclude_uri_pattern)
+    if not matches_exclude_pattern then
+      return ngx.redirect("https://" .. ngx.var.host .. ngx.var.request_uri, ngx.HTTP_MOVED_PERMANENTLY)
+    end
   end  
 end
 
