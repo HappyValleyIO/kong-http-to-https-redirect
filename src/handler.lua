58a8c70da7ec8443b3d0b0b4fec8fb520bb427ad
Dirk Steinkopf <dirk@steinkopf.net>
2018-11-26 05:39:41 +0100

    only redirect if x-forwared-for is not set to https

--- src/handler.lua
+++ src/handler.lua
@@ -14,7 +14,7 @@ end
 function HttpFilterHandler:access(conf)
   HttpFilterHandler.super.access(self)
 
-  if ngx.var.https ~= "on" then
+  if ngx.var.https ~= "on" and ngx.var.http_x_forwarded_proto ~= "https" then
     local matches_exclude_pattern = conf.exclude_uri_pattern and string.find(ngx.var.request_uri, conf.exclude_uri_pattern)
     if not matches_exclude_pattern then
       return ngx.redirect("https://" .. ngx.var.host .. ngx.var.request_uri, ngx.HTTP_MOVED_PERMANENTLY)
